fd5292b52eda539e8c38c0c6495358e691c1614b
MeghaveeG <katyanee0309@gmail.com>
2025-04-01 23:43:08 +0530

    Add files via upload

---
+++ App1.js
@@ -0,0 +1,18 @@
+
+
+
+import React from 'react';
+import CertificateForm from './components/CertificateForm';
+import VerifyCertificate from './components/VerifyCertificate';
+
+function App() {
+    return (
+        <div>
+            <h1>Blockchain Certificates</h1>
+            <CertificateForm />
+            <VerifyCertificate />
+        </div>
+    );
+}
+
+export default App;
